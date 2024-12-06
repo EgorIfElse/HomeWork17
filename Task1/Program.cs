4681b8eab5d59f30549ccc6019e33e57da68b21a
e.goncharenko <e.goncharenko@GR-ILSKIY-PC212>
2024-12-06 07:27:26 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,123 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Bank<int> bankInt = new Bank<int>();
+        bankInt.InputInfo();
+        Console.WriteLine(bankInt.GetInfo());
+
+        Bank<string> bankString = new Bank<string>();
+        bankString.InputInfo();
+        Console.WriteLine(bankString.GetInfo());
+
+    }
+}
+class Bank<T>
+{
+    private T AccountNumber;
+    public T accountNumber
+    {
+        get { return AccountNumber; }
+        set
+        {
+            if (value != null)
+            {
+                AccountNumber = value;
+            }
+
+        }
+    }
+
+    private double Balance;
+    public double balance
+    {
+        get { return Balance; }
+        set
+        {
+            if (value > 0)
+            {
+                Balance = value;
+
+
+            }
+
+
+        }
+    }
+    private string FIO;
+    public string fio
+    {
+        get { return FIO; }
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                FIO = value;
+            }
+            else
+            {
+                Console.WriteLine("ФИО не может быть пустым, введите повторно");
+
+            }
+
+
+
+
+        }
+    }
+    public void InputInfo()
+    {
+        
+        while (true)
+        {
+            Console.WriteLine("Введите номер счета");
+            try
+            {
+                accountNumber = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+                break;
+            }
+
+            catch
+            {
+                Console.WriteLine("Ошибка.Некорректный ввод счета");
+            }
+        }
+
+        Console.WriteLine("Введите баланс");
+        try
+        {
+            balance = Convert.ToDouble(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка, по умолчанию баланс 0");
+            balance = 0;
+
+        }
+
+        Console.WriteLine("Введите ФИО");
+        while (true)
+        {
+
+            FIO = Console.ReadLine();
+            if (!string.IsNullOrEmpty(fio))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("ФИО не может быть пустым, введите повторно");
+            }
+
+
+        }
+
+
+
+    }
+    public string GetInfo()
+    {
+        return $"Гражданин {FIO} имеет баланс {Balance} на счете {AccountNumber}";
+    }
+
+}
